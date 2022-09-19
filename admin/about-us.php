fd08d8a12562df78364ee6031081b1a92b1782fa
Michael Sanji <akunbotak503@gmail.com>
2022-09-19 14:50:34 +0700

    ubah text right side

--- admin/about-us.php
+++ admin/about-us.php
@@ -22,7 +22,7 @@ if (strlen($_SESSION['sturecmsaid'] == 0)) {
 
   <head>
 
-    <title>Student Management System|| Update About Us</title>
+    <title>Update Tentang Kami</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css">
     <link rel="stylesheet" href="vendors/flag-icon-css/css/flag-icon.min.css">
@@ -54,20 +54,19 @@ if (strlen($_SESSION['sturecmsaid'] == 0)) {
         <div class="main-panel">
           <div class="content-wrapper">
             <div class="page-header">
-              <h3 class="page-title"> Update About Us </h3>
+              <h3 class="page-title"> Update Tentang Kami </h3>
               <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
                   <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
-                  <li class="breadcrumb-item active" aria-current="page"> Update About Us</li>
+                  <li class="breadcrumb-item active" aria-current="page">Tentang Kami</li>
                 </ol>
               </nav>
             </div>
             <div class="row">
-
               <div class="col-12 grid-margin stretch-card">
                 <div class="card">
                   <div class="card-body">
-                    <h4 class="card-title" style="text-align: center;">Update About Us</h4>
+                    <h4 class="card-title" style="text-align: center;">Tentang Kami</h4>
 
                     <form class="forms-sample" method="post">
                       <?php
@@ -80,11 +79,11 @@ if (strlen($_SESSION['sturecmsaid'] == 0)) {
                       if ($query->rowCount() > 0) {
                         foreach ($results as $row) {               ?>
                           <div class="form-group">
-                            <label for="exampleInputName1">Page Title:</label>
+                            <label for="exampleInputName1">Judul Halaman:</label>
                             <input type="text" name="pagetitle" value="<?php echo $row->PageTitle; ?>" class="form-control" required='true'>
                           </div>
                           <div class="form-group">
-                            <label for="exampleInputName1">Page Description:</label>
+                            <label for="exampleInputName1">Deskripsi Halaman:</label>
                             <textarea type="text" name="pagedes" class="form-control" required='true'><?php echo $row->PageDescription; ?></textarea>
                           </div>
                       <?php $cnt = $cnt + 1;
