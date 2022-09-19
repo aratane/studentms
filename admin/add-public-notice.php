b48377461a099387fb359fe9a20a9b64dd0b63f1
Michael Sanji <akunbotak503@gmail.com>
2022-09-19 15:33:36 +0700

    ubah judul notice

--- admin/add-public-notice.php
+++ admin/add-public-notice.php
@@ -2,35 +2,33 @@
 session_start();
 error_reporting(0);
 include('includes/dbconnection.php');
-if (strlen($_SESSION['sturecmsaid']==0)) {
+if (strlen($_SESSION['sturecmsaid'] == 0)) {
   header('location:logout.php');
-  } else{
-   if(isset($_POST['submit']))
-  {
- $nottitle=$_POST['nottitle'];
+} else {
+  if (isset($_POST['submit'])) {
+    $nottitle = $_POST['nottitle'];
 
- $notmsg=$_POST['notmsg'];
-$sql="insert into tblpublicnotice(NoticeTitle,NoticeMessage)values(:nottitle,:notmsg)";
-$query=$dbh->prepare($sql);
-$query->bindParam(':nottitle',$nottitle,PDO::PARAM_STR);
-$query->bindParam(':notmsg',$notmsg,PDO::PARAM_STR);
- $query->execute();
-   $LastInsertId=$dbh->lastInsertId();
-   if ($LastInsertId>0) {
-    echo '<script>alert("Notice has been added.")</script>';
-echo "<script>window.location.href ='add-public-notice.php'</script>";
-  }
-  else
-    {
-         echo '<script>alert("Something Went Wrong. Please try again")</script>';
+    $notmsg = $_POST['notmsg'];
+    $sql = "insert into tblpublicnotice(NoticeTitle,NoticeMessage)values(:nottitle,:notmsg)";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':nottitle', $nottitle, PDO::PARAM_STR);
+    $query->bindParam(':notmsg', $notmsg, PDO::PARAM_STR);
+    $query->execute();
+    $LastInsertId = $dbh->lastInsertId();
+    if ($LastInsertId > 0) {
+      echo '<script>alert("Pengumuman Ditambahkan.")</script>';
+      echo "<script>window.location.href ='add-public-notice.php'</script>";
+    } else {
+      echo '<script>alert("Ada yang salah, Tolong coba lagi.")</script>';
     }
-}
-  ?>
-<!DOCTYPE html>
-<html lang="en">
+  }
+?>
+  <!DOCTYPE html>
+  <html lang="en">
+
   <head>
-   
-    <title>Student  Management System|| Add Notice</title>
+
+    <title>Tambah Pengumuman</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css">
     <link rel="stylesheet" href="vendors/flag-icon-css/css/flag-icon.min.css">
@@ -44,48 +42,49 @@ echo "<script>window.location.href ='add-public-notice.php'</script>";
     <!-- endinject -->
     <!-- Layout styles -->
     <link rel="stylesheet" href="css/style.css" />
-    
+
   </head>
+
   <body>
     <div class="container-scroller">
       <!-- partial:partials/_navbar.html -->
-     <?php include_once('includes/header.php');?>
+      <?php include_once('includes/header.php'); ?>
       <!-- partial -->
       <div class="container-fluid page-body-wrapper">
         <!-- partial:partials/_sidebar.html -->
-      <?php include_once('includes/sidebar.php');?>
+        <?php include_once('includes/sidebar.php'); ?>
         <!-- partial -->
         <div class="main-panel">
           <div class="content-wrapper">
             <div class="page-header">
-              <h3 class="page-title">Add Notice </h3>
+              <h3 class="page-title">Tambah Pengumuman </h3>
               <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
                   <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
-                  <li class="breadcrumb-item active" aria-current="page"> Add Notice</li>
+                  <li class="breadcrumb-item active" aria-current="page"> Tambah Pengumuman</li>
                 </ol>
               </nav>
             </div>
             <div class="row">
-          
+
               <div class="col-12 grid-margin stretch-card">
                 <div class="card">
                   <div class="card-body">
-                    <h4 class="card-title" style="text-align: center;">Add Notice</h4>
-                   
+                    <h4 class="card-title" style="text-align: center;">Pengumuman Publik</h4>
+
                     <form class="forms-sample" method="post" enctype="multipart/form-data">
-                      
+
                       <div class="form-group">
-                        <label for="exampleInputName1">Notice Title</label>
+                        <label for="exampleInputName1">Judul Pengumuman</label>
                         <input type="text" name="nottitle" value="" class="form-control" required='true'>
                       </div>
                       <div class="form-group">
-                        <label for="exampleInputName1">Notice Message</label>
+                        <label for="exampleInputName1">Pesan Pengumuman</label>
                         <textarea name="notmsg" value="" class="form-control" required='true'></textarea>
                       </div>
-                   
-                      <button type="submit" class="btn btn-primary mr-2" name="submit">Add</button>
-                     
+
+                      <button type="submit" class="btn btn-primary mr-2" name="submit">Tambah</button>
+
                     </form>
                   </div>
                 </div>
@@ -94,7 +93,7 @@ echo "<script>window.location.href ='add-public-notice.php'</script>";
           </div>
           <!-- content-wrapper ends -->
           <!-- partial:partials/_footer.html -->
-         <?php include_once('includes/footer.php');?>
+          <?php include_once('includes/footer.php'); ?>
           <!-- partial -->
         </div>
         <!-- main-panel ends -->
@@ -118,4 +117,5 @@ echo "<script>window.location.href ='add-public-notice.php'</script>";
     <script src="js/select2.js"></script>
     <!-- End custom js for this page -->
   </body>
-</html><?php }  ?>
+
+  </html><?php }  ?>
