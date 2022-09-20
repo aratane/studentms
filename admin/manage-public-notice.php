679c5e5e9fd83b958d514faf8bb6b1655481860e
Michael Sanji <akunbotak503@gmail.com>
2022-09-20 12:27:57 +0700

    add icon edit

--- admin/manage-public-notice.php
+++ admin/manage-public-notice.php
@@ -2,28 +2,26 @@
 session_start();
 error_reporting(0);
 include('includes/dbconnection.php');
-if (strlen($_SESSION['sturecmsaid']==0)) {
+if (strlen($_SESSION['sturecmsaid'] == 0)) {
   header('location:logout.php');
-  } else{
-   // Code for deletion
-if(isset($_GET['delid']))
-{
-$rid=intval($_GET['delid']);
-$sql="delete from tblpublicnotice where ID=:rid";
-$query=$dbh->prepare($sql);
-$query->bindParam(':rid',$rid,PDO::PARAM_STR);
-$query->execute();
- echo "<script>alert('Data deleted');</script>"; 
-  echo "<script>window.location.href = 'manage-public-notice.php'</script>";     
-
-
-}
+} else {
+  // Code for deletion
+  if (isset($_GET['delid'])) {
+    $rid = intval($_GET['delid']);
+    $sql = "delete from tblpublicnotice where ID=:rid";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':rid', $rid, PDO::PARAM_STR);
+    $query->execute();
+    echo "<script>alert('Data Dihapus.');</script>";
+    echo "<script>window.location.href = 'manage-public-notice.php'</script>";
+  }
 ?>
-<!DOCTYPE html>
-<html lang="en">
+  <!DOCTYPE html>
+  <html lang="en">
+
   <head>
-   
-    <title>Student  Management System|||Manage Public Notice</title>
+
+    <title>Kelola Pengumuman Publik</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="vendors/simple-line-icons/css/simple-line-icons.css">
     <link rel="stylesheet" href="vendors/flag-icon-css/css/flag-icon.min.css">
@@ -38,25 +36,26 @@ $query->execute();
     <!-- Layout styles -->
     <link rel="stylesheet" href="./css/style.css">
     <!-- End layout styles -->
-   
+
   </head>
+
   <body>
     <div class="container-scroller">
       <!-- partial:partials/_navbar.html -->
-     <?php include_once('includes/header.php');?>
+      <?php include_once('includes/header.php'); ?>
       <!-- partial -->
       <div class="container-fluid page-body-wrapper">
         <!-- partial:partials/_sidebar.html -->
-        <?php include_once('includes/sidebar.php');?>
+        <?php include_once('includes/sidebar.php'); ?>
         <!-- partial -->
         <div class="main-panel">
           <div class="content-wrapper">
-             <div class="page-header">
-              <h3 class="page-title"> Manage Public Notice </h3>
+            <div class="page-header">
+              <h3 class="page-title"> Kelola Pengumuman </h3>
               <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
                   <li class="breadcrumb-item"><a href="dashboard.php">Dashboard</a></li>
-                  <li class="breadcrumb-item active" aria-current="page"> Manage Public Notice</li>
+                  <li class="breadcrumb-item active" aria-current="page"> Kelola Pengumuman</li>
                 </ol>
               </nav>
             </div>
@@ -65,71 +64,83 @@ $query->execute();
                 <div class="card">
                   <div class="card-body">
                     <div class="d-sm-flex align-items-center mb-4">
-                      <h4 class="card-title mb-sm-0">Manage Public Notice</h4>
-                      <a href="#" class="text-dark ml-auto mb-3 mb-sm-0"> View all Public Notice</a>
+                      <h4 class="card-title mb-sm-0">Kelola Data Pengumuman</h4>
+                      <a href="#" class="text-dark ml-auto mb-3 mb-sm-0"> Lihat Semua Pengumuman</a>
                     </div>
                     <div class="table-responsive border rounded p-1">
                       <table class="table">
                         <thead>
                           <tr>
-                            <th class="font-weight-bold">S.No</th>
-                            <th class="font-weight-bold">Notice Title</th>
-                            <th class="font-weight-bold">Notice Date</th>
-                            <th class="font-weight-bold">Action</th>
-                            
+                            <th class="font-weight-bold">NO</th>
+                            <th class="font-weight-bold">Judul Pengumuman</th>
+                            <th class="font-weight-bold">Tanggal Pengumuman</th>
+                            <th class="font-weight-bold">Aksi</th>
+
                           </tr>
                         </thead>
                         <tbody>
-                           <?php
-                            if (isset($_GET['pageno'])) {
-            $pageno = $_GET['pageno'];
-        } else {
-            $pageno = 1;
-        }
-        // Formula for pagination
-        $no_of_records_per_page =15;
-        $offset = ($pageno-1) * $no_of_records_per_page;
-       $ret = "SELECT ID FROM tblpublicnotice";
-$query1 = $dbh -> prepare($ret);
-$query1->execute();
-$results1=$query1->fetchAll(PDO::FETCH_OBJ);
-$total_rows=$query1->rowCount();
-$total_pages = ceil($total_rows / $no_of_records_per_page);
-$sql="SELECT * from tblpublicnotice";
-$query = $dbh -> prepare($sql);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
+                          <?php
+                          if (isset($_GET['pageno'])) {
+                            $pageno = $_GET['pageno'];
+                          } else {
+                            $pageno = 1;
+                          }
+                          // Formula for pagination
+                          $no_of_records_per_page = 15;
+                          $offset = ($pageno - 1) * $no_of_records_per_page;
+                          $ret = "SELECT ID FROM tblpublicnotice";
+                          $query1 = $dbh->prepare($ret);
+                          $query1->execute();
+                          $results1 = $query1->fetchAll(PDO::FETCH_OBJ);
+                          $total_rows = $query1->rowCount();
+                          $total_pages = ceil($total_rows / $no_of_records_per_page);
+                          $sql = "SELECT * from tblpublicnotice";
+                          $query = $dbh->prepare($sql);
+                          $query->execute();
+                          $results = $query->fetchAll(PDO::FETCH_OBJ);
 
-$cnt=1;
-if($query->rowCount() > 0)
-{
-foreach($results as $row)
-{               ?>   
-                          <tr>
-                           
-                            <td><?php echo htmlentities($cnt);?></td>
-                            <td><?php  echo htmlentities($row->NoticeTitle);?></td>
-                            <td><?php  echo htmlentities($row->CreationDate);?></td>
-                            <td>
-                              <div><a href="edit-public-notice-detail.php?editid=<?php echo htmlentities ($row->ID);?>"><i class="icon-eye"></i></a>
-                                                || <a href="manage-public-notice.php?delid=<?php echo ($row->ID);?>" onclick="return confirm('Do you really want to Delete ?');"> <i class="icon-trash"></i></a></div>
-                            </td> 
-                          </tr><?php $cnt=$cnt+1;}} ?>
+                          $cnt = 1;
+                          if ($query->rowCount() > 0) {
+                            foreach ($results as $row) {               ?>
+                              <tr>
+
+                                <td><?php echo htmlentities($cnt); ?></td>
+                                <td><?php echo htmlentities($row->NoticeTitle); ?></td>
+                                <td><?php echo htmlentities($row->CreationDate); ?></td>
+                                <td>
+                                  <div><a href="edit-public-notice-detail.php?editid=<?php echo htmlentities($row->ID); ?>"><i class="icon-eye"></i></a>
+                                    || <a href="manage-public-notice.php?delid=<?php echo ($row->ID); ?>" onclick="return confirm('Apakah Kamu Yakin Ingin Menghapus ?');"> <i class="icon-trash"></i></a></div>
+                                </td>
+                              </tr><?php $cnt = $cnt + 1;
+                                  }
+                                } ?>
                         </tbody>
                       </table>
                     </div>
                     <div align="left">
-    <ul class="pagination" >
-        <li><a href="?pageno=1"><strong>First></strong></a></li>
-        <li class="<?php if($pageno <= 1){ echo 'disabled'; } ?>">
-            <a href="<?php if($pageno <= 1){ echo '#'; } else { echo "?pageno=".($pageno - 1); } ?>"><strong style="padding-left: 10px">Prev></strong></a>
-        </li>
-        <li class="<?php if($pageno >= $total_pages){ echo 'disabled'; } ?>">
-            <a href="<?php if($pageno >= $total_pages){ echo '#'; } else { echo "?pageno=".($pageno + 1); } ?>"><strong style="padding-left: 10px">Next></strong></a>
-        </li>
-        <li><a href="?pageno=<?php echo $total_pages; ?>"><strong style="padding-left: 10px">Last</strong></a></li>
-    </ul>
-</div>
+                      <ul class="pagination">
+                        <li><a href="?pageno=1"><strong>Pertama></strong></a></li>
+                        <li class="<?php if ($pageno <= 1) {
+                                      echo 'disabled';
+                                    } ?>">
+                          <a href="<?php if ($pageno <= 1) {
+                                      echo '#';
+                                    } else {
+                                      echo "?pageno=" . ($pageno - 1);
+                                    } ?>"><strong style="padding-left: 10px">Sebelumnya></strong></a>
+                        </li>
+                        <li class="<?php if ($pageno >= $total_pages) {
+                                      echo 'disabled';
+                                    } ?>">
+                          <a href="<?php if ($pageno >= $total_pages) {
+                                      echo '#';
+                                    } else {
+                                      echo "?pageno=" . ($pageno + 1);
+                                    } ?>"><strong style="padding-left: 10px">Selanjutnya></strong></a>
+                        </li>
+                        <li><a href="?pageno=<?php echo $total_pages; ?>"><strong style="padding-left: 10px">Terakhir></strong></a></li>
+                      </ul>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -137,7 +148,7 @@ foreach($results as $row)
           </div>
           <!-- content-wrapper ends -->
           <!-- partial:partials/_footer.html -->
-         <?php include_once('includes/footer.php');?>
+          <?php include_once('includes/footer.php'); ?>
           <!-- partial -->
         </div>
         <!-- main-panel ends -->
@@ -162,4 +173,5 @@ foreach($results as $row)
     <script src="./js/dashboard.js"></script>
     <!-- End custom js for this page -->
   </body>
-</html><?php }  ?>
+
+  </html><?php }  ?>
