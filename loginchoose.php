3ff8d3b5a944184db9cea2ed1ed20e0cf11a82d9
Michael Sanji <akunbotak503@gmail.com>
2022-09-27 11:11:57 +0700

    ganti image

--- loginchoose.php
+++ loginchoose.php
@@ -7,17 +7,15 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
   <link rel="stylesheet" href="style.css">
-
 </head>
 
 <body>
-  <!-- partial:index.partial.html -->
   <div class="cards">
     <h3>IGAPIN STUDENT MANAGEMENT</h3>
     <h1>Login Sebagai ...</h1>
     <div class="card card__one">
       <div class="card__bg"></div>
-      <a href="admin/login.php"><img class="card__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/62105/3dr_mono.png" /></a>
+      <a href="admin/login.php"><img class="card__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/62105/3dr_howlcastle.png" /></a>
       <div class="card__text">
         <p class="card__title">ADMIN (Petugas)</p>
       </div>
@@ -31,17 +29,14 @@
     </div>
     <div class="card card__three">
       <div class="card__bg"></div>
-      <a href="teacher/login.php"><img class="card__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/62105/3dr_howlcastle.png" /></a>
+      <a href="teacher/login.php"><img class="card__img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/62105/3dr_mono.png" /></a>
       <div class="card__text">
         <p class="card__title">TEACHER (Guru)</p>
       </div>
     </div>
   </div>
-
   <a class="twitter__link" href="index.php"><img class="twitter__icon" src="https://img.icons8.com/3d-fluency/100/000000/left.png" />Kembali Ke Website</a>
-  <!-- partial -->
   <script src="script.js"></script>
-
 </body>
 
 </html>
